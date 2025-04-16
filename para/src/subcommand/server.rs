c05f6da64aae9c791e94a6caeaf4f6c55674133f
paratoxic <paratoxic@parasite.dev>
2025-04-16 13:49:32 -0400

    Fmt

--- para/src/subcommand/server.rs
+++ para/src/subcommand/server.rs
@@ -18,7 +18,8 @@ impl Server {
             .nest_service("/pool/", ServeDir::new(log_dir.join("pool")))
             .nest_service("/users/", ServeDir::new(log_dir.join("users")));
 
-        router = router.layer(ServiceBuilder::new()
+        router = router.layer(
+            ServiceBuilder::new()
                 .layer(SetResponseHeaderLayer::overriding(
                     CONTENT_TYPE,
                     HeaderValue::from_static("text/plain"),
@@ -26,7 +27,8 @@ impl Server {
                 .layer(SetResponseHeaderLayer::if_not_present(
                     CONTENT_DISPOSITION,
                     HeaderValue::from_static("inline"),
-                )));
+                )),
+        );
 
         self.spawn(router, handle, self.address.clone(), self.port)?
             .await??;
