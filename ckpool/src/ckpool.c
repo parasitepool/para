224575ace3e6c7d91cad1fa5b264e919663bbbea
paratoxic <paratoxic@parasite.dev>
2025-04-21 19:11:43 -0400

    Amend

--- ckpool/src/ckpool.c
+++ ckpool/src/ckpool.c
@@ -1810,6 +1810,12 @@ int main(int argc, char** argv) {
         if (fork())
             exit(0);
         setsid();
+        fd = open("/dev/null", O_RDWR, 0);
+        if (fd != -1) {
+            dup2(fd, STDIN_FILENO);
+            dup2(fd, STDOUT_FILENO);
+            dup2(fd, STDERR_FILENO);
+        }
     }
 
     write_namepid(&ckp.main);
