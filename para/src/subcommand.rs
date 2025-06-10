d689114bd321e3568189b5f8042d7361935cbf43
parabit <parabit@parasite.dev>
2025-06-10 16:27:20 -0500

    draft

--- para/src/subcommand.rs
+++ para/src/subcommand.rs
@@ -1,11 +1,18 @@
 use super::*;
 
 mod server;
+mod sync;
 
 #[derive(Debug, Parser)]
 pub(crate) enum Subcommand {
     #[command(about = "Run API server")]
     Server(server::Server),
+
+    #[command(about = "Send shares to ZMQ endpoint")]
+    SyncSend(sync::SyncSend),
+
+    #[command(about = "Receive and process shares from ZMQ endpoint")]
+    SyncReceive(sync::SyncReceive),
 }
 
 impl Subcommand {
@@ -15,6 +22,21 @@ impl Subcommand {
                 let handle = Handle::new();
 
                 Runtime::new()?.block_on(async { server.run(options, handle).await.unwrap() });
+
+                Ok(())
+            }
+            Self::SyncSend(sync_send) => {
+                let handle = Handle::new();
+
+                Runtime::new()?.block_on(async { sync_send.run(options, handle).await.unwrap() });
+
+                Ok(())
+            }
+            Self::SyncReceive(sync_receive) => {
+                let handle = Handle::new();
+
+                Runtime::new()?
+                    .block_on(async { sync_receive.run(options, handle).await.unwrap() });
 
                 Ok(())
             }
