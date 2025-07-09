f10f50f9276137f47f3ab56e2d8843bdcde97e83
paratoxicdev <paratoxic@parasite.dev>
2025-07-09 17:29:04 -0400

    Write current pool transactions to file (#94)

--- ckpool/src/ckpool.c
+++ ckpool/src/ckpool.c
@@ -1483,16 +1483,27 @@ static void prepare_child(ckpool_t* ckp, proc_instance_t* pi, void* process, cha
     create_unix_receiver(pi);
 }
 
-static struct option long_options[] = {{"btcsolo", no_argument, 0, 'B'},        {"config", required_argument, 0, 'c'},
-                                       {"daemonise", no_argument, 0, 'D'},      {"group", required_argument, 0, 'g'},
-                                       {"handover", no_argument, 0, 'H'},       {"help", no_argument, 0, 'h'},
-                                       {"killold", no_argument, 0, 'k'},        {"log-shares", no_argument, 0, 'L'},
-                                       {"loglevel", required_argument, 0, 'l'}, {"name", required_argument, 0, 'n'},
-                                       {"node", no_argument, 0, 'N'},           {"passthrough", no_argument, 0, 'P'},
-                                       {"proxy", no_argument, 0, 'p'},          {"quiet", no_argument, 0, 'q'},
-                                       {"redirector", no_argument, 0, 'R'},     {"sockdir", required_argument, 0, 's'},
-                                       {"trusted", no_argument, 0, 't'},        {"userproxy", no_argument, 0, 'u'},
-                                       {"signet", no_argument, 0, 'S'},         {0, 0, 0, 0}};
+static struct option long_options[] = {{"btcsolo", no_argument, 0, 'B'},
+                                       {"config", required_argument, 0, 'c'},
+                                       {"daemonise", no_argument, 0, 'D'},
+                                       {"group", required_argument, 0, 'g'},
+                                       {"handover", no_argument, 0, 'H'},
+                                       {"help", no_argument, 0, 'h'},
+                                       {"killold", no_argument, 0, 'k'},
+                                       {"log-shares", no_argument, 0, 'L'},
+                                       {"loglevel", required_argument, 0, 'l'},
+                                       {"name", required_argument, 0, 'n'},
+                                       {"node", no_argument, 0, 'N'},
+                                       {"passthrough", no_argument, 0, 'P'},
+                                       {"proxy", no_argument, 0, 'p'},
+                                       {"quiet", no_argument, 0, 'q'},
+                                       {"redirector", no_argument, 0, 'R'},
+                                       {"sockdir", required_argument, 0, 's'},
+                                       {"trusted", no_argument, 0, 't'},
+                                       {"userproxy", no_argument, 0, 'u'},
+                                       {"signet", no_argument, 0, 'S'},
+                                       {"log-txns", no_argument, 0, 'T'},
+                                       {0, 0, 0, 0}};
 
 static bool send_recv_path(const char* path, const char* msg) {
     int   sockd = open_unix_client(path);
@@ -1536,7 +1547,7 @@ int main(int argc, char** argv) {
     if (!strcmp(appname, "ckproxy"))
         ckp.proxy = true;
 
-    while ((c = getopt_long(argc, argv, "Bc:Dd:g:HhkLl:Nn:PpqRS:s:tu", long_options, &i)) != -1) {
+    while ((c = getopt_long(argc, argv, "Bc:Dd:g:HhkLl:Nn:PpqRST:s:tu", long_options, &i)) != -1) {
         switch (c) {
             case 'B':
                 if (ckp.proxy)
@@ -1625,6 +1636,9 @@ int main(int argc, char** argv) {
                 break;
             case 'S':
                 ckp.signet = true;
+                break;
+            case 'T':
+                ckp.logtxns = true;
                 break;
         }
     }
