2d45d2bd05c479c6e0360d2593c54f994400940e
Parabit <parabit@parasite.dev>
2025-07-30 15:51:25 -0400

    sync execution targets (#119)
    * include syncsend and syncreceive in server with shutdown signalling
    
    * add long-running mode to syncsend as default with option to terminate when out of new shares
    
    * allow 5555 for zmq listener on aggregation node
    
    * bind to external ip
    
    * to_string instead of into
    
    * avoid setting our current_id to a record that no longer exists
    
    * reduce log spam
    
    * add zmq_receiver flag
    
    * use hostname instead of zmq-receiver flag

--- src/subcommand/server/config.rs
+++ src/subcommand/server/config.rs
@@ -30,6 +30,8 @@ pub(crate) struct Config {
     password: Option<String>,
     #[clap(long, help = "Collect statistics from <NODES>.")]
     nodes: Vec<Url>,
+    #[arg(long, help = "Send shares to ZMQ endpoint <ZMQ_SEND_ENDPOINT>")]
+    zmq_endpoint: Option<String>,
 }
 
 impl Config {
@@ -95,5 +97,9 @@ impl Config {
 
     pub(crate) fn nodes(&self) -> Vec<Url> {
         self.nodes.clone()
+    }
+
+    pub(crate) fn zmq_endpoint(&self) -> Option<String> {
+        self.zmq_endpoint.clone()
     }
 }
