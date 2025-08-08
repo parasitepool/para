f72322fce7b32ed70f3ef3b5204e9662f938778a
paratoxic <paratoxic@parasite.dev>
2025-08-08 19:12:44 -0400

    Fix

--- tests/server.rs
+++ tests/server.rs
@@ -1,36 +1,6 @@
 use super::*;
 
 #[test]
-fn run() {
-    let port = TcpListener::bind("127.0.0.1:0")
-        .unwrap()
-        .local_addr()
-        .unwrap()
-        .port();
-
-    let mut child = CommandBuilder::new(format!("server --address 127.0.0.1 --port {port}"))
-        .integration_test(true)
-        .spawn();
-
-    for attempt in 0.. {
-        if let Ok(response) = reqwest::blocking::get(format!("http://127.0.0.1:{port}")) {
-            if response.status() == 200 {
-                break;
-            }
-        }
-
-        if attempt == 100 {
-            panic!("Server did not respond to status check",);
-        }
-
-        thread::sleep(Duration::from_millis(50));
-    }
-
-    child.kill().unwrap();
-    child.wait().unwrap();
-}
-
-#[test]
 fn serve_stats() {
     let _server = TestServer::spawn();
 }
