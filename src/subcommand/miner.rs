5c3d6b79d127731dc745dab91b803a3782e08515
paratoxic <paratoxic@parasite.dev>
2025-08-19 08:47:11 -0700

    Consolidate

--- src/subcommand/miner.rs
+++ src/subcommand/miner.rs
@@ -13,7 +13,6 @@ pub(crate) struct Miner {
     username: String,
     #[arg(long, help = "Stratum <PASSWORD>")]
     password: String,
-    // add flag to exit on share submission or block found
 }
 
 impl Miner {
