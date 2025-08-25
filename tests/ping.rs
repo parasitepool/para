3337b2545fa1b5e4e5f059797bfd954dafe041b8
paratoxic <paratoxic@parasite.dev>
2025-08-25 13:45:04 -0400

    Delete some tests

--- tests/ping.rs
+++ tests/ping.rs
@@ -5,18 +5,6 @@ fn signet_username() -> String {
 }
 
 #[test]
-fn ping_with_count() {
-    let ckpool = TestCkpool::spawn();
-    let stratum_endpoint = ckpool.stratum_endpoint();
-
-    let mut ping =
-        CommandBuilder::new(format!("ping --count 5 --timeout 1 {stratum_endpoint}")).spawn();
-
-    let exit_status = ping.wait().unwrap();
-    assert_eq!(exit_status.code(), Some(0));
-}
-
-#[test]
 fn ping() {
     let ckpool = TestCkpool::spawn();
 
@@ -70,29 +58,7 @@ fn ping_with_username_and_password() {
 }
 
 #[test]
-fn ping_target_without_port() {
-    let mut ping = CommandBuilder::new("ping --count 1 --timeout 1 127.0.0.1").spawn();
-
-    let exit_status = ping.wait().unwrap();
-
-    assert_eq!(exit_status.code(), Some(1));
-}
-
-#[test]
-fn ping_port_fallback_behavior() {
-    let mut ping_no_port = CommandBuilder::new("ping --count 1 --timeout 1 127.0.0.1").spawn();
-    let mut ping_explicit_port =
-        CommandBuilder::new("ping --count 1 --timeout 1 127.0.0.1:42069").spawn();
-
-    let exit_status_no_port = ping_no_port.wait().unwrap();
-    let exit_status_explicit = ping_explicit_port.wait().unwrap();
-
-    assert_eq!(exit_status_no_port.code(), exit_status_explicit.code());
-    assert_eq!(exit_status_no_port.code(), Some(1));
-}
-
-#[test]
-fn ping_invalid_hostname() {
+fn ping_invalid_hostname_fails() {
     let mut ping =
         CommandBuilder::new("ping --count 1 --timeout 1 invalid.hostname.that.does.not.exist")
             .spawn();
