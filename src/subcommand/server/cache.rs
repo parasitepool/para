fe15073ccd44b8f07b519c05f9678aaef6a27804
paratoxic <paratoxic@parasite.dev>
2025-09-26 18:47:52 -0400

    Aggregate users

--- src/subcommand/server/cache.rs
+++ src/subcommand/server/cache.rs
@@ -35,7 +35,8 @@ pub(super) struct Cache {
     client: Client,
     config: Arc<ServerConfig>,
     pool_status: Mutex<Cached<ckpool::Status>>,
-    users: DashMap<String, Arc<Mutex<Cached<ckpool::User>>>>,
+    user_statuses: DashMap<String, Arc<Mutex<Cached<ckpool::User>>>>,
+    users: Mutex<Cached<Vec<String>>>,
 }
 
 impl Cache {
@@ -44,7 +45,8 @@ impl Cache {
             client,
             config: config.clone(),
             pool_status: Mutex::new(Cached::init(config.ttl())),
-            users: DashMap::new(),
+            user_statuses: DashMap::new(),
+            users: Mutex::new(Cached::init(config.ttl())),
         }
     }
 
@@ -94,7 +96,7 @@ impl Cache {
 
     pub(super) async fn user_status(&self, address: String) -> Result<Option<ckpool::User>> {
         let cell = self
-            .users
+            .user_statuses
             .entry(address.clone())
             .or_insert_with(|| Arc::new(Mutex::new(Cached::init(self.config.ttl()))))
             .clone();
@@ -137,6 +139,47 @@ impl Cache {
         if aggregated.is_none() {
             error!("Failed to find user {address} on any node");
         }
+
+        *cached = Cached::new(aggregated.clone());
+
+        Ok(aggregated)
+    }
+
+    pub(super) async fn users(&self) -> Result<Option<Vec<String>>> {
+        let mut cached = self.users.lock().await;
+        if cached.is_fresh(self.config.ttl()) {
+            return Ok(cached.value());
+        }
+
+        let nodes = self.config.nodes();
+        let fetches = nodes.iter().map(|url| {
+            let client = self.client.clone();
+            async move {
+                let result = async {
+                    let resp = client.get(url.join("/users")?).send().await?;
+                    serde_json::from_str::<Vec<String>>(&resp.text().await?).map_err(Into::into)
+                }
+                .await;
+                (url, result)
+            }
+        });
+
+        let results: Vec<(&Url, Result<Vec<String>>)> = join_all(fetches).await;
+
+        let mut set = HashSet::new();
+        for (url, result) in results {
+            match result {
+                Ok(users) => set.extend(users),
+                Err(err) => warn!("Failed to fetch status from {url} with: {err}"),
+            }
+        }
+
+        let aggregated = if set.is_empty() {
+            error!("Failed aggregate users");
+            None
+        } else {
+            Some(set.into_iter().collect::<Vec<String>>())
+        };
 
         *cached = Cached::new(aggregated.clone());
 
