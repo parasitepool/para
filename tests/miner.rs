75adb0533454951127b55cbe2ca982938743bddc
paratoxic <paratoxic@parasite.dev>
2025-10-02 10:21:13 -0400

    Solidify

--- tests/miner.rs
+++ tests/miner.rs
@@ -3,5 +3,16 @@ use super::*;
 #[test]
 #[ignore]
 fn miner() {
-    todo!()
+    let pool = TestPool::spawn();
+
+    let stratum_endpoint = pool.stratum_endpoint();
+
+    let mut miner = CommandBuilder::new(format!(
+        "miner --once --username {} {stratum_endpoint}",
+        signet_username()
+    ))
+    .spawn();
+
+    let exit_status = miner.wait().unwrap();
+    assert_eq!(exit_status.code(), Some(0));
 }
