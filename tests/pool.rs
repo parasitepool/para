c9953e828498aef976a4ba581bbdde14db17c935
paratoxicdev <paratoxic@parasite.dev>
2025-10-22 16:26:42 -0400

    Add some tests for various things (#252)

--- tests/pool.rs
+++ tests/pool.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 #[test]
-fn pool_is_pingable() {
+fn ping_pool() {
     let pool = TestPool::spawn();
 
     let stratum_endpoint = pool.stratum_endpoint();
@@ -14,7 +14,7 @@ fn pool_is_pingable() {
 }
 
 #[test]
-fn pool_update_interval() {
+fn configure_template_update_interval() {
     let pool = TestPool::spawn_with_args("--update-interval 1");
 
     let stratum_endpoint = pool.stratum_endpoint();
@@ -42,4 +42,117 @@ fn pool_update_interval() {
     let t2 = serde_json::from_str::<Template>(&String::from_utf8_lossy(&output.stdout)).unwrap();
 
     assert!(t1.ntime < t2.ntime);
+}
+
+#[tokio::test]
+async fn basic_initialization_flow() {
+    let pool = TestPool::spawn();
+
+    let mut client = pool.stratum_client().await;
+
+    let (subscribe, _, _) = client.subscribe().await.unwrap();
+
+    assert_eq!(subscribe.subscriptions.len(), 2);
+
+    assert!(client.authorize().await.is_ok());
+
+    let set_difficulty = match client.incoming.recv().await.unwrap() {
+        Message::Notification { method: _, params } => {
+            serde_json::from_value::<SetDifficulty>(params).unwrap()
+        }
+        _ => panic!(),
+    };
+
+    assert!(set_difficulty.difficulty() < Difficulty::from(0.01));
+
+    let notify = match client.incoming.recv().await.unwrap() {
+        Message::Notification { method: _, params } => {
+            serde_json::from_value::<Notify>(params).unwrap()
+        }
+        _ => panic!(),
+    };
+
+    assert_eq!(notify.job_id, JobId::from(0));
+    assert!(notify.clean_jobs);
+}
+
+#[tokio::test]
+async fn configure_with_multiple_negotiation_steps() {
+    let pool = TestPool::spawn();
+
+    let mut client = pool.stratum_client().await;
+
+    assert!(
+        client
+            .configure(vec!["unknown-extension".into()], None)
+            .await
+            .unwrap_err()
+            .to_string()
+            .contains("Unsupported extension")
+    );
+
+    assert!(
+        client
+            .configure(
+                vec!["version-rolling".into()],
+                Some(Version::from_str("1fffe000").unwrap())
+            )
+            .await
+            .is_ok()
+    );
+
+    assert!(
+        client
+            .configure(
+                vec!["version-rolling".into()],
+                Some(Version::from_str("1fffe111").unwrap())
+            )
+            .await
+            .is_ok()
+    );
+
+    let (subscribe, _, _) = client.subscribe().await.unwrap();
+
+    assert_eq!(subscribe.subscriptions.len(), 2);
+
+    assert!(client.authorize().await.is_ok());
+}
+
+#[tokio::test]
+async fn authorize_before_subscribe_fails() {
+    let pool = TestPool::spawn();
+
+    let mut client = pool.stratum_client().await;
+
+    assert!(
+        client
+            .authorize()
+            .await
+            .unwrap_err()
+            .to_string()
+            .contains("Method not allowed in current state")
+    );
+}
+
+#[tokio::test]
+async fn submit_before_authorize_fails() {
+    let pool = TestPool::spawn();
+
+    let mut client = pool.stratum_client().await;
+
+    client.subscribe().await.unwrap();
+
+    assert!(
+        client
+            .submit(
+                JobId::new(3),
+                Extranonce::generate(8),
+                Ntime::from(0),
+                Nonce::from(12345),
+            )
+            .await
+            .unwrap_err()
+            .to_string()
+            .contains("Unauthorized")
+    );
 }
