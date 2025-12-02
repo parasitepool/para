346e82e34605ef76ef0b2477f2c8817c9ad62dfd
paratoxic <paratoxic@parasite.dev>
2025-12-02 13:45:46 -0500

    Fix

--- tests/ping.rs
+++ tests/ping.rs
@@ -23,47 +23,57 @@ fn ping_ckpool() {
 
     let stratum_endpoint = ckpool.stratum_endpoint();
 
-    let mut ping =
-        CommandBuilder::new(format!("ping --count 1 --timeout 1 {stratum_endpoint}")).spawn();
-
-    let exit_status = ping.wait().unwrap();
-
-    assert_eq!(exit_status.code(), Some(0));
-
-    let mut ping = CommandBuilder::new("ping --count 1 --timeout 1 127.0.0.1:1234").spawn();
-
-    let exit_status = ping.wait().unwrap();
-
-    assert_eq!(exit_status.code(), Some(1));
-
     let username = signet_username();
 
-    let mut ping = CommandBuilder::new(format!(
-        "ping --count 1 --timeout 1 --username {username} {stratum_endpoint}"
-    ))
-    .spawn();
+    assert_eq!(
+        CommandBuilder::new(format!("ping --count 1 --timeout 1 {stratum_endpoint}"))
+            .spawn()
+            .wait()
+            .unwrap()
+            .code(),
+        Some(0)
+    );
 
-    let exit_status = ping.wait().unwrap();
-    assert_eq!(exit_status.code(), Some(0));
+    assert_eq!(
+        CommandBuilder::new("ping --count 1 --timeout 1 127.0.0.1:1234")
+            .spawn()
+            .wait()
+            .unwrap()
+            .code(),
+        Some(1)
+    );
 
-    let mut ping = CommandBuilder::new(format!(
+    assert_eq!(
+        CommandBuilder::new(format!(
+            "ping --count 1 --timeout 1 --username {username} {stratum_endpoint}"
+        ))
+        .spawn()
+        .wait()
+        .unwrap()
+        .code(),
+        Some(0)
+    );
+
+    assert_eq!(CommandBuilder::new(format!(
         "ping --count 1 --timeout 1 --username {username} --password testpass {stratum_endpoint}"
     ))
-    .spawn();
+    .spawn().wait().unwrap().code(), Some(0));
 
-    let exit_status = ping.wait().unwrap();
-    assert_eq!(exit_status.code(), Some(0));
-
-    let mut ping =
+    assert_eq!(
         CommandBuilder::new("ping --count 1 --timeout 1 invalid.hostname.that.does.not.exist")
-            .spawn();
+            .spawn()
+            .wait()
+            .unwrap()
+            .code(),
+        Some(1)
+    );
 
-    let exit_status = ping.wait().unwrap();
-    assert_eq!(exit_status.code(), Some(1));
-
-    let mut ping =
-        CommandBuilder::new(format!("ping --count 3 --timeout 1 {stratum_endpoint}")).spawn();
-
-    let exit_status = ping.wait().unwrap();
-    assert_eq!(exit_status.code(), Some(0));
+    assert_eq!(
+        CommandBuilder::new(format!("ping --count 3 --timeout 1 {stratum_endpoint}"))
+            .spawn()
+            .wait()
+            .unwrap()
+            .code(),
+        Some(0)
+    );
 }
