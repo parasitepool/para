00158674b27dc1ed86680e662725f4afcdaf2517
parachemist <parachemist@parasite.dev>
2025-12-04 02:17:49 -0500

    feat & refactor: Rename template_ckpool and add test
    Renamed template_ckpool to template_raw & added a new interpreted template test.

--- tests/template.rs
+++ tests/template.rs
@@ -3,7 +3,32 @@ use super::*;
 #[test]
 #[serial(bitcoind)]
 #[timeout(90000)]
-fn template_ckpool() {
+fn template_raw() {
+    let ckpool = TestCkpool::spawn();
+
+    let stratum_endpoint = ckpool.stratum_endpoint();
+
+    let template = CommandBuilder::new(format!(
+        "template {stratum_endpoint} --username {} --raw",
+        signet_username()
+    ))
+    .spawn();
+
+    let stdout = template.wait_with_output().unwrap();
+    let output =
+        serde_json::from_str::<Template>(&String::from_utf8_lossy(&stdout.stdout)).unwrap();
+
+    assert!(output.merkle_branches.is_empty());
+    assert_eq!(output.extranonce2_size, 8);
+    assert_eq!(output.ip_address, "127.0.0.1".to_string());
+
+    assert_eq!(stdout.status.code(), Some(0));
+}
+
+#[test]
+#[serial(bitcoind)]
+#[timeout(90000)]
+fn template_interpreted() {
     let ckpool = TestCkpool::spawn();
 
     let stratum_endpoint = ckpool.stratum_endpoint();
@@ -16,11 +41,13 @@ fn template_ckpool() {
 
     let stdout = template.wait_with_output().unwrap();
     let output =
-        serde_json::from_str::<Template>(&String::from_utf8_lossy(&stdout.stdout)).unwrap();
+        serde_json::from_str::<InterpretedTemplate>(&String::from_utf8_lossy(&stdout.stdout))
+            .unwrap();
 
-    assert!(output.merkle_branches.is_empty());
-    assert_eq!(output.extranonce2_size, 8);
+    assert_eq!(output.mining_params.extranonce2_size, 8);
     assert_eq!(output.ip_address, "127.0.0.1".to_string());
+    assert!(output.coinbase.block_height.is_some());
+    assert!(output.block_header.difficulty > 0.0);
 
     assert_eq!(stdout.status.code(), Some(0));
 }
