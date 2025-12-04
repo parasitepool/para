2f83819f90326b55bbb71e9c87fc3ec7d7fc8c36
paratoxic <paratoxic@parasite.dev>
2025-12-03 19:05:16 -0500

    Update

--- src/hash_rate.rs
+++ src/hash_rate.rs
@@ -18,17 +18,17 @@ const SI_PREFIXES: &[(&str, f64)] = &[
 ];
 
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Default)]
-pub struct HashRate(pub f64);
+pub(crate) struct HashRate(pub f64);
 
 impl HashRate {
-    pub const ZERO: Self = Self(0.0);
+    pub(crate) const ZERO: Self = Self(0.0);
 
-    pub fn from_dsps(dsps: f64) -> Self {
+    pub(crate) fn from_dsps(dsps: f64) -> Self {
         Self(dsps * HASHES_PER_DIFF_1 as f64)
     }
 
     #[cfg(test)]
-    pub fn estimate(total_difficulty: f64, window: Duration) -> Self {
+    pub(crate) fn estimate(total_difficulty: f64, window: Duration) -> Self {
         if window.is_zero() {
             return Self::ZERO;
         }
