f3edf959a09bd559d305f4c169b472d9a764815f
paratoxic <paratoxic@parasite.dev>
2026-01-29 12:34:30 -0500

    Address review comments

--- src/settings/proxy_options.rs
+++ src/settings/proxy_options.rs
@@ -29,12 +29,6 @@ pub(crate) struct ProxyOptions {
     #[arg(long, help = "Load Bitcoin Core data dir from <BITCOIN_DATA_DIR>.")]
     pub(crate) bitcoin_data_dir: Option<PathBuf>,
 
-    #[arg(
-        long,
-        help = "Authenticate to Bitcoin Core RPC with <BITCOIN_RPC_PASSWORD>."
-    )]
-    pub(crate) bitcoin_rpc_password: Option<String>,
-
     #[arg(long, help = "Connect to Bitcoin Core RPC at <BITCOIN_RPC_PORT>.")]
     pub(crate) bitcoin_rpc_port: Option<u16>,
 
@@ -43,6 +37,12 @@ pub(crate) struct ProxyOptions {
         help = "Authenticate to Bitcoin Core RPC as <BITCOIN_RPC_USERNAME>."
     )]
     pub(crate) bitcoin_rpc_username: Option<String>,
+
+    #[arg(
+        long,
+        help = "Authenticate to Bitcoin Core RPC with <BITCOIN_RPC_PASSWORD>."
+    )]
+    pub(crate) bitcoin_rpc_password: Option<String>,
 
     #[arg(long, help = "Load Bitcoin Core RPC cookie file from <COOKIE_FILE>.")]
     pub(crate) bitcoin_rpc_cookie_file: Option<PathBuf>,
